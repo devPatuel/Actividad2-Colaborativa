67a33ce33b86637584a83875d73b3683fe8b5bad
Angelery33 <angelcantero99@gmail.com>
2025-09-26 20:40:29 +0200

    Ahi lo llevais

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -1,28 +1,41 @@
-package com.example.androidproject
+package com.example.prueba3
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidproject.ui.theme.AndroidProjectTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.prueba3.ui.theme.Prueba3Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AndroidProjectTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+            Surface (
+                modifier=Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background,
+            )
+            {
+
+                Prueba3Theme {
+                    GreetingText(
+                        message = "Happy Birthday Sam!",
+                        from = "From Angelery",
+                        modifier = Modifier.padding(100.dp)
                     )
                 }
             }
@@ -31,17 +44,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
+fun GreetingText(message:String,from : String,  modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier,
+    ) {
+        Text(
+            text = message,
+            fontSize = 100.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center,
+
+
+            )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End),
+            textAlign = TextAlign.Right,
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    AndroidProjectTheme {
-        Greeting("Android")
+    Surface (
+        modifier=Modifier.fillMaxSize(),
+        color = MaterialTheme.colorScheme.background,
+    )
+    {
+
+        Prueba3Theme {
+            GreetingText(
+                message = "Happy Birthday Sam!",
+                from = "From Angelery",
+
+                )
+        }
     }
 }
