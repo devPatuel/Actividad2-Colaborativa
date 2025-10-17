a1b488f0d4f9b8f0da25f13b74e8e698e9245257
ncondeviladev <ncondeviladev@gmail.com>
2025-10-17 20:53:29 +0200

    Commit final Noe

--- app/src/main/java/com/example/androidproject/MainActivity.kt
+++ app/src/main/java/com/example/androidproject/MainActivity.kt
@@ -32,7 +32,7 @@ class MainActivity : ComponentActivity() {
                 Prueba3Theme {
                     GreetingText(
                         message = "Happy Birthday Angel Cantero!",
-                        from = "From Papa Noel",
+                        from = "Noé Conde",
                         modifier = Modifier.padding(100.dp)
                     )
                 }
